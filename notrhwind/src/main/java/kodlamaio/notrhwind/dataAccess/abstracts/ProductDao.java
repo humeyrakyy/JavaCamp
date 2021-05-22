f968d2bd9c37860ce9929b149145d674425d62c9
humeyrakyy <humeyrakoyun3@gmail.com>
2021-05-23 00:44:02 +0300

    7.Gün

---
+++ notrhwind/src/main/java/kodlamaio/notrhwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,11 @@
+package kodlamaio.notrhwind.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.notrhwind.entities.concretes.Product;
+
+
+public interface ProductDao extends JpaRepository<Product,Integer> {
+        
+	
+}

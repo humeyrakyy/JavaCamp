82c384ba36a9a707d778109ec966727283faf7ca
humeyrakyy <humeyrakoyun3@gmail.com>
2021-05-23 00:41:26 +0300

    7.Gün

---
+++ notrhwind/notrhwind/src/main/java/kodlamaio/notrhwind/NotrhwindApplication.java
@@ -0,0 +1,13 @@
+package kodlamaio.notrhwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotrhwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotrhwindApplication.class, args);
+	}
+
+}
